5113d9c2ded482e8f6c41d3b465e4f53150bf87c
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-04 20:08:04 +0530

    Added:To create all the details of the person

---
+++ Address__Book/Address__Book/AdressBook.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address__Book
+{
+    interface IContactDetails
+    {
+         void ValidateContactDetails();
+    }
+    class Contacts : IContactDetails
+    {
+        //variables
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private int zipCode;
+        private long phoneNumber;
+        private string email;
+
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public void ValidateContactDetails()
+        {
+            bool fullName = ValidateFullName(FirstName, LastName);
+            bool address = ValidateAddress(Address, City, State, ZipCode);
+            bool contact = ValidateContactNumbers(PhoneNumber, Email);
+            if (fullName == true && address == true && contact == true)
+            {
+                Console.WriteLine("contact saved succesfully");
+            }
+            else
+                Console.WriteLine("error in saving contact");
+            
+        }
+        private bool ValidateFullName(string firstName, string lastName)
+        {
+            if (firstName != "Jones" && lastName != "Martin")
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool ValidateAddress(string Address, string City, string State, int  ZipCode)
+        {
+            if (Address != "some road" && City != "goa" && State != "goa" && ZipCode != 123 )
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool ValidateContactNumbers(long PhoneNumber, string Email)
+        {
+            if (PhoneNumber != 6547882683 && Email != "xyz@mail")
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+        
+}
+            
+                
+
+            
+           
+            
+           
+            
--- Address__Book/Address__Book/Program.cs
+++ Address__Book/Address__Book/Program.cs
@@ -7,6 +7,23 @@ namespace Address__Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Sysyem");
+            Contacts contacts = new Contacts();
+            contacts.FirstName = "Sairam";
+            contacts.LastName = "Kadakuntla";
+            contacts.PhoneNumber = 987654321;
+            contacts.State = "Telangana";
+            contacts.City = "Hyderabad";
+            contacts.Email = "abcd@gmail.com";
+            contacts.ZipCode = 500028;
+            contacts.ValidateContactDetails();
+            Console.WriteLine("Contact Details: ");
+            Console.WriteLine("Full Name" +contacts.FirstName+"  "+ contacts.LastName);
+            Console.WriteLine("Phone Number="+contacts.PhoneNumber);
+            Console.WriteLine("Address ="+contacts.City+ "  " +contacts.State+ "  " +contacts.ZipCode);
+            Console.WriteLine("Email Id =" + contacts.Email);
+
+           
+
         }
     }
 }
